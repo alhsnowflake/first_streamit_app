57786c6f0022f32b6d3e637bf8f91acc0d2a030d
HardyParty <115280766+alhsnowflake@users.noreply.github.com>
2022-10-07 16:41:53 -0500

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -41,23 +41,31 @@ try:
  # streamlit.error()
   #streamlit.write('The user entered ', fruit_choice)
 
-# don't run anything past here
-streamlit.stop()
-
-import pandas
-import requests
 import snowflake.connector
 from urllib.error import URLError
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("use warehouse pc_rivery_wh")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+
+streamlit.header("This fruit load list contains:")
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+#my_cur = my_cnx.cursor()
+#my_cur.execute("use warehouse pc_rivery_wh")
+
+#my_data_row = my_cur.fetchone()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_row)
 
 #New section to display fruityvice api responce
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
