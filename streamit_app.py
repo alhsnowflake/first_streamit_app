b11a66a632d6520e678bb0797efc97c1c7ce484d
HardyParty <115280766+alhsnowflake@users.noreply.github.com>
2022-10-07 16:34:39 -0500

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -21,6 +21,11 @@ fruits_to_show = my_fruit_list.loc[fruits_seleceted]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#create the repeatable code block
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
 #New section to display fruityvice api responce
 streamlit.header("Fruityvice Fruit Advice!")
@@ -29,15 +34,11 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #streamlit.text(fruityvice_response.json())
-    # take the json version of the responce and normalize it
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # output the screen as a table
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
   
-except URLError as e:
-  streamlit.error()
+#except URLError as e:
+ # streamlit.error()
   #streamlit.write('The user entered ', fruit_choice)
 
 # don't run anything past here
