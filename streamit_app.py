c8751f0fb25221a15abfc564de725d7d3fdeba5f
HardyParty <115280766+alhsnowflake@users.noreply.github.com>
2022-10-07 16:15:08 -0500

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -43,3 +43,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+#New section to display fruityvice api responce
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
