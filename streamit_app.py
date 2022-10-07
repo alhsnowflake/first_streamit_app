749e9b665c886c5b649514398b0effce91b2c82a
HardyParty <115280766+alhsnowflake@users.noreply.github.com>
2022-10-07 16:22:25 -0500

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+#import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -35,7 +35,13 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here
+streamlit.stop()
+
+import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("use warehouse pc_rivery_wh")
